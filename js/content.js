88671df42297c6d2a28dc400ef3079fd08c95f0a
kogleron <kogleron@gmail.com>
2020-03-10 09:38:03 +0300

    Fix code style

--- js/content.js
+++ js/content.js
@@ -1,14 +1,12 @@
-var s = document.createElement( 'script' );
+const s = document.createElement( 'script' );
 s.src = chrome.extension.getURL( 'js/page.js' );
 (document.head || document.documentElement).appendChild( s );
 s.parentNode.removeChild( s );
 
 chrome.runtime.onMessage.addListener( function ( req ) {
-    switch ( req.type ) {
-        case "browserActionPressed":
-            let event = new CustomEvent( 'yard-pause.playPause' );
-            window.dispatchEvent( event );
-            break;
+    if (req.type === "browserActionPressed") {
+        let event = new CustomEvent( 'yard-pause.playPause' );
+        window.dispatchEvent( event );
     }
 } );
 
